ea93cc9876a25eb5b1f05c8b66b1a5c5e34780cc
aaerni123 <aaerni@unomaha.edu>
2024-09-18 11:27:57 -0500

    Lab 4

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name:Alivia Aerni
+#Date:9/18/24
+#Assignment:Lab 4
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -11,6 +11,14 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
+def squareInSquare(myTurtle, num):
+    size = 10
+    for i in range(num):
+        drawSquare(myTurtle, size)
+        size = size + 20
+        myTurtle.right(180)
+        myTurtle.forward(10)
+        
 
 def main():
     myTurtle = turtle.Turtle()
@@ -20,7 +28,7 @@ def main():
     # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+    squaresInSquares(myTurtle, 4) #draws 5 concentric squares
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
